10bd8da475c709e44df07d6c9f7b7db2fef550e7
song199210 <1821908096@qq.com>
2018-08-22 09:35:25 +0800

    实现记住账号密码

--- web/src/views/Login/login.js
+++ web/src/views/Login/login.js
@@ -3,18 +3,38 @@ import md5 from "js-md5";
 import { Form, Icon, Input, Button, Checkbox,message } from 'antd';
 import "./login.scss";
 const FormItem = Form.Item;
-
+const storage=window.localStorage;
 class NormalLoginForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      isChk:true
+    }
+  }
+  componentDidMount(){
+    this.props.form.setFieldsValue({
+      "uname":storage.getItem("uname")?storage.getItem("uname"):"",
+      "pwd":storage.getItem("pwd")?storage.getItem("pwd"):""
+    })
+  }
   handleSubmit = (e) => {
     e.preventDefault();
     const {form,history}=this.props;
+    const {isChk}=this.state;
     form.validateFields((err, values) => {
+      if(isChk){//保存账号密码
+        storage.setItem("uname",values.uname);
+        storage.setItem("pwd",values.pwd);
+      }else{//不保存账号密码
+        storage.removeItem("uname");
+        storage.removeItem("pwd");
+      }
       values.pwd=md5(values.pwd);
       if (!err) {
         window.$common.httpAjax("login","POST",values).then((res)=>{
                 if(res.flag === "success"){
                   message.success(res.msg);
-                  window.localStorage.setItem("userId",res.data.userid);
+                  storage.setItem("userId",res.data.userid);
                   history.push("/admin");
                 }else{
                   message.error(res.msg);
@@ -25,8 +45,15 @@ class NormalLoginForm extends React.Component {
       }
     });
   }
+  rUserInfo=(evt)=>{
+    const tar=evt.target;
+    this.setState({
+      isChk:tar.checked
+    })
+  }
   render() {
     const { getFieldDecorator } = this.props.form;
+    const {isChk} = this.state;
     return (
       <div className="loginBox">
         <div className="container">
@@ -49,9 +76,9 @@ class NormalLoginForm extends React.Component {
             <FormItem>
               {getFieldDecorator('remember', {
                 valuePropName: 'checked',
-                initialValue: true,
+                initialValue: isChk,
               })(
-                <Checkbox>记住密码</Checkbox>
+                <Checkbox onChange={(evt)=>{this.rUserInfo(evt)}}>记住密码</Checkbox>
               )}
               <div>
                 <Button style={{"width":"100%"}} type="primary" htmlType="submit" className="login-form-button">登录</Button>
