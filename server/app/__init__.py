17d786afb04f6845664b4a1c001f68ec43b99526
song199210 <1821908096@qq.com>
2018-09-05 18:12:59 +0800

    修复bug

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 from flask_mail import Mail
 from flask_cors import CORS
 from pymysql import install_as_MySQLdb
-from logging import basicConfig
 
 install_as_MySQLdb() #解决MySQLdb不支持python3.5的问题
 
@@ -14,9 +13,6 @@ mail=Mail(app) #初始化邮箱组件
 
 
 path=os.path.dirname(__file__)#配置日志
-file_name=os.path.join(path,"loggin.txt")
-basicConfig(filename=file_name,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
 #解决跨域问题
 CORS(app,support_credentials=True)
 
--- server/home.py
+++ server/home.py
@@ -1,11 +1,8 @@
 import sys
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
+from gevent.pywsgi import WSGIServer
 from app import app
 
 if __name__ == "__main__":
     print("服务器启动!!!!!")
-    server = HTTPServer(WSGIContainer(app))
-    server.listen(5000)
-    IOLoop.instance().start()
+    server = WSGIServer(("localhost",5000),app)
+    server.serve_forever()
